462e6cb41acb8067429693ee2e042e3113f90798
tarek <tarek421995@gmail.com>
2020-11-11 22:15:26 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBk_CfxGuonOGFdNtFw-JvtXf0qzZpbg5U",
+    authDomain: "whatsapp-web-tarek.firebaseapp.com",
+    databaseURL: "https://whatsapp-web-tarek.firebaseio.com",
+    projectId: "whatsapp-web-tarek",
+    storageBucket: "whatsapp-web-tarek.appspot.com",
+    messagingSenderId: "1051391341925",
+    appId: "1:1051391341925:web:c8d84e22eacc9119915d8c",
+    measurementId: "G-2HNLGBFDN0"
+};  
+  
